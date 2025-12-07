b7d4a0f9e5715e0976cde67a7e7620e36b40fc91
Petar Iliev <petariliev@Petars-MacBook-Pro-2.local>
2025-12-07 10:44:25 -0800

    Day 7: DP

--- advent-of-code-7.py
+++ advent-of-code-7.py
@@ -4,40 +4,39 @@ def main():
     # Step 1: Parse input
     file_name = sys.argv[1]
     input = []
+    start_col = 0
     with open(file_name, "r") as file:
         for line in file:
-            input_row = [char for char in line.strip()]
+            line = line.strip()
+            input_row = []
+            for i in range(len(line)):
+                char = line[i]
+                if char == "S":
+                    star_col = i
+                    input_row.append(1)
+                elif char == ".":
+                    input_row.append(0)
+                else:
+                    input_row.append("^")
             input.append(input_row)
 
-    num_splits = [0]
-    
-    # Step 2: Define search
-    def dfs(row, col):
-        # Out of range 
-        if row >= len(input) or col < 0 or col >= len(input[0]):
-            return
+    # Step 2: Perform dp
+    for i, current_row in enumerate(input):
+        if i == 0:
+            continue
         
-        # Already processed
-        current = input[row][col]
-        if current == "|": 
-            return
-        
-        # Empty space, move down
-        if current == "S" or current == ".":
-            input[row][col] = "|"
-            dfs(row + 1, col)
-        # Split
-        elif current == "^":
-            num_splits[0] += 1
-            dfs(row, col + 1)
-            dfs(row, col - 1)
+        prev_row = input[i - 1]
+        for j in range(len(current_row)):
+            # Special case
+            if current_row[j] == "^":
+                current_row[j - 1] += prev_row[j]
+                current_row[j + 1] += prev_row[j]
+                current_row[j] = 0
+            else:
+                current_row[j] += prev_row[j]
 
-    # Step 3: Find starting position
-    for i in range(len(input[0])):
-        if input[0][i] == "S":
-            dfs(0, i)
-    
-    print(num_splits[0])
+    return sum(input[-1])
 
 if __name__ == "__main__":
-    main()
+    result = main()
+    print(result)
